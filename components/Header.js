eafa9ec48c3523b42ef9437074016c4d0bd6bdaf
Joshua Callaway <joshua.callaway@dynamon.co.uk>
2022-03-18 09:23:22 +0000

    Opacity animation added to logo

--- components/Header.js
+++ components/Header.js
@@ -8,10 +8,10 @@ import {
 import Image from "next/image";
 import ProfilePic from "../public/profile_image.png";
 import { useRouter } from "next/router";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export const Header = () => {
-  const router = useRouter()
+  const router = useRouter();
   // get time and date so welcome messages
   let date = new Date();
   let time = date.getHours();
@@ -24,6 +24,12 @@ export const Header = () => {
   } else {
     message = "Good evening";
   }
+
+  const variants = {
+    visible: { opacity: 1 },
+    hidden: { opacity: 0 },
+  };
+
   return (
     <header className="flex flex-col bg-another-blue dark:bg-new-blue min-h-screen">
       <div className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
@@ -33,11 +39,21 @@ export const Header = () => {
           <NavItem link="#projects" title="Work" Icon={BriefcaseIcon} />
           <NavItem link="#contact" title="Contact" Icon={MailIcon} />
         </nav>
-        <motion.div initial={{ x: +200}} animate={{ x: 0 }} transition={{ duration: 2 }} className="hero-feature-text text-xl mt-3 drop-shadow-md text-josh-yellow sm:mr-6">
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          transition={{ duration: 3}}
+          className="hero-feature-text text-xl mt-3 drop-shadow-md text-josh-yellow sm:mr-6"
+        >
           fullStackJosh
         </motion.div>
       </div>
-      <motion.div animate={{ rotate: 360 }} transition={{ duration: 1 }} className="flex flex-col items-center ml-3 mr-3 mt-6 mb-5">
+      <motion.div
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1 }}
+        className="flex flex-col items-center ml-3 mr-3 mt-6 mb-5"
+      >
         <Image
           src={ProfilePic}
           width="210px"
@@ -48,11 +64,16 @@ export const Header = () => {
       </motion.div>
       <article className="flex flex-col items-center m-5">
         <h1 className="mx-4 p-2 text-6xl font-bold text-center	leading-tight md:text-7xl md:px-32 lg:px-52">
-          {message} Im <strong className="hero-feature-text text-josh-yellow drop-shadow-md">Josh</strong>
+          {message} Im{" "}
+          <strong className="hero-feature-text text-josh-yellow drop-shadow-md">
+            Josh
+          </strong>
         </h1>
         <p className="mx-4 mt-12 text-3xl text-center">
-          <strong className="hero-feature-text text-josh-yellow drop-shadow-md">Full Stack</strong> Developer
-          from the United Kingdom
+          <strong className="hero-feature-text text-josh-yellow drop-shadow-md">
+            Full Stack
+          </strong>{" "}
+          Developer from the United Kingdom
         </p>
       </article>
     </header>
